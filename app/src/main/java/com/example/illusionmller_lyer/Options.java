2584b6e36c5b03a28a110e25b182eb4a7fac65ee
AxelCarayon <axel.carayon@etud.univ-jfc.fr>
2020-01-28 19:58:12 +0100

    Ecriture des options dans un fichier properties

--- app/src/main/java/com/example/illusionmller_lyer/Options.java
+++ app/src/main/java/com/example/illusionmller_lyer/Options.java
@@ -3,27 +3,44 @@ package com.example.illusionmller_lyer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class Options extends AppCompatActivity {
 
+    Button button_valider;
+    int nb_entrainement;
+    int nb_exercices;
+    long seed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+        //instantiation valeurs ====================================================================
+        button_valider = findViewById(R.id.button_valider_options); //bouton valider
         SeekBar nombre_essais = findViewById(R.id.seekbar_nb_essais); //seekbar exercice
         SeekBar nombre_essais_entrainement = findViewById(R.id.seekbar_nb_essais_entrainement); //seekbar entrainement
-        final TextView valeur_nb_essais_exercice = findViewById(R.id.textView_nb_essais_valeur);
-        final TextView valeur_nb_essais_entrainement = findViewById(R.id.textView_nb_essais_valeur_entrainement);
+        final TextView valeur_nb_essais_exercice = findViewById(R.id.textView_nb_essais_valeur); //valeur exo
+        final TextView valeur_nb_essais_entrainement = findViewById(R.id.textView_nb_essais_valeur_entrainement); //valeur entrainement
+        final EditText text_seed = findViewById(R.id.editText_seed); //valeur seed
 
-        //listener seekbar exercice
+        seed = 0;
+        nb_entrainement = 0;
+        nb_exercices = 0;
+        //==========================================================================================
+
+        //listener seekbar exercice ================================================================
         nombre_essais.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress,
                                           boolean fromUser) {
                 valeur_nb_essais_exercice.setText(String.valueOf(progress));
+                nb_exercices = progress;
             }
 
             @Override
@@ -34,13 +51,29 @@ public class Options extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+        //==========================================================================================
 
-        //listener seekbar entrainement
+        //listener bouton valider ==================================================================
+        button_valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ReadWrite_File file = new ReadWrite_File("/options.properties");
+                file.write(nb_entrainement,nb_exercices,seed);
+                String[] res = file.read();
+                Log.i("entrainement",""+res[0]);
+                Log.i("exercice",""+res[1]);
+                Log.i("seed",""+res[2]);
+            }
+        });
+        //==========================================================================================
+
+        //listener seekbar entrainement ============================================================
         nombre_essais_entrainement.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress,
                                           boolean fromUser) {
                 valeur_nb_essais_entrainement.setText(String.valueOf(progress));
+                nb_entrainement = progress;
             }
 
             @Override
@@ -51,6 +84,7 @@ public class Options extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+        //==========================================================================================
     }
 }
 
---
+++ app/src/main/java/com/example/illusionmller_lyer/ReadWrite_File.java
@@ -0,0 +1,90 @@
+package com.example.illusionmller_lyer;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+class ReadWrite_File {
+
+    String filepath;
+
+    public ReadWrite_File(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public void write(int entrainement, int exercice, long seed){
+        File root = android.os.Environment.getExternalStorageDirectory();
+        File dir = new File (root.getAbsolutePath() + "/IllusionMullerLyer");
+        dir.mkdirs();
+        File properties =  new File(dir+filepath);
+        try {
+            properties.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (checkExternalMedia()) {
+            try (OutputStream output = new FileOutputStream(dir+filepath)) {
+                Properties prop = new Properties();
+
+                // set the properties value
+                prop.setProperty("nb_entrainement", String.valueOf(entrainement));
+                prop.setProperty("nb_exercice", String.valueOf(exercice));
+                prop.setProperty("seed", String.valueOf(seed));
+
+                // save properties to project root folder
+                prop.store(output, "test");
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+        }
+
+    }
+    public String[] read(){
+        String[] resultat = new String[3];
+        File root = android.os.Environment.getExternalStorageDirectory();
+        File dir = new File (root.getAbsolutePath() + "/IllusionMullerLyer");
+        //Log.i("properties",dir+filepath);
+        if (checkExternalMedia()) {
+            try (InputStream output = new FileInputStream(dir+filepath)) {
+                Properties prop = new Properties();
+
+                // set the properties value
+                resultat[0] = prop.getProperty("nb_entrainement");
+                resultat[1] = prop.getProperty("nb_exercice");
+                resultat[2] = prop.getProperty("seed");
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+        }
+        return resultat;
+    }
+
+    private boolean checkExternalMedia(){
+        boolean mExternalStorageAvailable = false;
+        boolean mExternalStorageWriteable = false;
+        String state = Environment.getExternalStorageState();
+
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            // Can read and write the media
+            mExternalStorageAvailable = mExternalStorageWriteable = true;
+        } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            // Can only read the media
+            mExternalStorageAvailable = true;
+            mExternalStorageWriteable = false;
+        } else {
+            // Can't read or write
+            mExternalStorageAvailable = mExternalStorageWriteable = false;
+        }
+        //Log.i("medias","\n\nExternal Media: readable="
+        //        +mExternalStorageAvailable+" writable="+mExternalStorageWriteable);
+        return mExternalStorageWriteable;
+    }
+
+}
