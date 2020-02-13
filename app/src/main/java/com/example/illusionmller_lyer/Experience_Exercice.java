a96d9ec66d9402f796327748c0e7eca3004bd094
AxelCarayon <axel.carayon@etud.univ-jfc.fr>
2020-02-13 14:54:19 +0100

    Ajout du timer pour l'exercice

--- app/src/main/java/com/example/illusionmller_lyer/Experience_Exercice.java
+++ app/src/main/java/com/example/illusionmller_lyer/Experience_Exercice.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
 public class Experience_Exercice extends AppCompatActivity {
 
+    private Handler handler;
+    private boolean visible;
     private Button button_egal;
     private Button button_plus;
     private Button button_moins;
@@ -18,6 +21,7 @@ public class Experience_Exercice extends AppCompatActivity {
     private int nbRepetitions;
     private long seed;
     private boolean secondeSerie;
+    private int duree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +56,7 @@ public class Experience_Exercice extends AppCompatActivity {
         final int[] options = lireOptions();
         nbRepetitions = options[0];
         seed = options[1];
+        duree = options[2];
         secondeSerie = false;
         draw_canvasExercice = new Draw_Canvas_Exercice(this,0.6,seed,nbRepetitions);
         canvas = findViewById(R.id.canvas_exercice);
@@ -59,6 +64,8 @@ public class Experience_Exercice extends AppCompatActivity {
         button_plus=findViewById(R.id.button_plus_exo);
         button_moins=findViewById(R.id.button_exo_moins);
         button_egal=findViewById(R.id.button_egal_exo);
+        handler = new Handler();
+        timer(duree);
         //==========================================================================================
         button_moins.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,10 +79,10 @@ public class Experience_Exercice extends AppCompatActivity {
                     else{
                         nbRepetitions = options[0];
                         secondeSerie = true;
-                        draw_canvasExercice.invalidate();
+                        timer(duree);
                     }
                 }else{
-                    draw_canvasExercice.invalidate();
+                    timer(duree);
                 }
             }
         });
@@ -92,10 +99,10 @@ public class Experience_Exercice extends AppCompatActivity {
                     else{
                         nbRepetitions = options[0];
                         secondeSerie = true;
-                        draw_canvasExercice.invalidate();
+                        timer(duree);
                     }
                 }else{
-                    draw_canvasExercice.invalidate();
+                    timer(duree);
                 }
             }
         });
@@ -112,10 +119,10 @@ public class Experience_Exercice extends AppCompatActivity {
                     else{
                         nbRepetitions = options[0];
                         secondeSerie = true;
-                        draw_canvasExercice.invalidate();
+                        timer(duree);
                     }
                 }else{
-                    draw_canvasExercice.invalidate();
+                    timer(duree);
                 }
             }
         });
@@ -123,13 +130,15 @@ public class Experience_Exercice extends AppCompatActivity {
 
     private int[] lireOptions(){
         ReadWrite_File file = new ReadWrite_File("/options.properties");
-        int[] res = new int[2];
+        int[] res = new int[3];
         try{
             res[0] = Integer.parseInt(file.read()[1]);
             res[1] = Integer.parseInt(file.read()[2]);
+            res[2] = Integer.parseInt(file.read()[3]);
         }catch (Exception e){
             res[0] = 5;
             res[1] = 0;
+            res[2] = 3;
         }
         return res;
     }
@@ -140,5 +149,24 @@ public class Experience_Exercice extends AppCompatActivity {
         intent.putExtra("attendu",draw_canvasExercice.getAttendu());
         startActivity(intent);
         finish();
+    }
+
+    private void timer(int ms){
+        handler.removeCallbacksAndMessages(null);
+        int interval = ms * 1000; // 1 Second
+        if (visible){
+            draw_canvasExercice.invalidate();
+        }
+        Runnable runnable = new Runnable(){
+            public void run() {
+                canvas.setVisibility(View.GONE);
+                visible = false;
+            }
+        };
+        handler.postAtTime(runnable, System.currentTimeMillis()+interval);
+        handler.postDelayed(runnable, interval);
+        canvas.setVisibility(View.VISIBLE);
+        visible = true;
+
     }
 }
