47949a9c0fd380566f496210dbaf77cd01781bdb
AxelCarayon <axel.carayon@etud.univ-jfc.fr>
2020-02-25 13:38:02 +0100

    Correction bug crash lors du demande de permissions

--- app/src/main/java/com/example/illusionmller_lyer/Menu_Principal.java
+++ app/src/main/java/com/example/illusionmller_lyer/Menu_Principal.java
@@ -18,52 +18,50 @@ public class Menu_Principal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if (isStoragePermissionGranted()) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_menu_principal);
-            final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-            getWindow().getDecorView().setSystemUiVisibility(flags);
+        super.onCreate(savedInstanceState);
 
-            final View decorView = getWindow().getDecorView();
-            decorView
-                    .setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
-                    {
+        isStoragePermissionGranted();
 
-                        @Override
-                        public void onSystemUiVisibilityChange(int visibility)
-                        {
-                            if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
-                            {
-                                decorView.setSystemUiVisibility(flags);
-                            }
+        setContentView(R.layout.activity_menu_principal);
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(flags);
+
+        final View decorView = getWindow().getDecorView();
+        decorView
+                .setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            decorView.setSystemUiVisibility(flags);
                         }
-                    });
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-            button_options = findViewById(R.id.button_options);
-            button_commencer = findViewById(R.id.button_commencer);
+                    }
+                });
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        button_options = findViewById(R.id.button_options);
+        button_commencer = findViewById(R.id.button_commencer);
 
-            //listener bouton commencer
-            button_commencer.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openNewActivity(Consigne_Entrainement.class);
-                }
-            });
+        //listener bouton commencer
+        button_commencer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity(Consigne_Entrainement.class);
+            }
+        });
 
 
-            //listener du bouton commencer
-            button_options.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openNewActivity(Options.class);
-                }
-            });
-        }
+        //listener du bouton commencer
+        button_options.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity(Options.class);
+            }
+        });
     }
 
     public void openNewActivity(Class activite) {
